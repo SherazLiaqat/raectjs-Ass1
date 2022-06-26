81f793b16d80bdf0542b655c911a518966627e84
SherazLiaqat <78857931+SherazLiaqat@users.noreply.github.com>
2022-06-26 18:40:51 +0500

    screen+components added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-
-import './App.css';
-
+import "./App.css";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import Home from "./Screens/Home";
+import Draft from "./Screens/Draft";
+import Archive from "./Screens/Archive";
 function App() {
   return (
     <>
-    <h1>hello</h1>
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/archive">Archive</Link>
+            </li>
+            <li>
+              <Link to="/draft">Draft</Link>
+            </li>
+          </ul>
+
+          <hr />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/archive" element={<Archive />} />
+            <Route path="/draft" element={<Draft />} />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
